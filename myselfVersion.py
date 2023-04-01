3e08f6f1273e729dc7364e85fe155c4188bd116c
first <it-is-post@yandex.ru>
2023-04-01 13:11:07 +0400

    Показывает работу

--- myselfVersion.py
+++ myselfVersion.py
@@ -1,17 +1,26 @@
-import socks
-import socket
+import requests
 
+url = r'https://check-host.net/ip'
+headers = {'user-agent': 'Mozilla/'
+                         '5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/'
+                         '20091102 Firefox/'
+                         '3.5.5 (.NET CLR 3.5.30729)'}
 with open("lt.txt", 'r', encoding='utf8') as f:
     proxy_list = [line.strip() for line in f]
 for proxy in proxy_list:
     host, port = proxy.split(':')
+    proxy_dic = {
+        "http": f'socks4://{host}:{port}',
+        "https": f'socks4://{host}:{port}'
+    }
+
+    print(proxy_dic)
     try:
-        s = socks.socksocket(socket.AF_INET, socket.SOCK_STREAM)
-        s.set_proxy(socks.SOCKS4, host ,int(port))
-        s.settimeout(20)
-        s.connect(("http://httpbin.org/ip", 80))
-        print(s.recv(4096))
+        res = requests.get(url, headers=headers, proxies=proxy_dic)
+        print(res.status_code, res.text)
     except Exception as e:
         print(f'{proxy} is not working: {e}')
 
+    print("end")
 
+#"http": f'socks4://{host}:{port}',
