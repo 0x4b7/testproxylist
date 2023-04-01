349d3d9adf24da91cd580346d2fc8eef1e73dbb9
first <it-is-post@yandex.ru>
2023-04-01 14:14:34 +0400

    Показывает работу

--- myselfVersion.py
+++ myselfVersion.py
@@ -5,8 +5,10 @@ headers = {'user-agent': 'Mozilla/'
                          '5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/'
                          '20091102 Firefox/'
                          '3.5.5 (.NET CLR 3.5.30729)'}
+
 with open("lt.txt", 'r', encoding='utf8') as f:
     proxy_list = [line.strip() for line in f]
+list_ready=''
 for proxy in proxy_list:
     host, port = proxy.split(':')
     proxy_dic = {
@@ -17,10 +19,12 @@ for proxy in proxy_list:
     print(proxy_dic)
     try:
         res = requests.get(url, headers=headers, proxies=proxy_dic)
-        print(res.status_code, res.text)
+        print('Статус подключения',res.status_code,'Адрес:', res.text)
+        list_ready+=host+':'+ port+'\n'
     except Exception as e:
         print(f'{proxy} is not working: {e}')
 
-    print("end")
+with open("workedProxis.txt",'w',encoding='utf8') as fw:
+    fw.write(list_ready)
 
-#"http": f'socks4://{host}:{port}',
+
